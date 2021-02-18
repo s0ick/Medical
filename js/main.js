79ffd4e575ea9a43729ea7fb50f68b7dd70e3c24
unknown <danilabrehov@gmail.com>
2021-02-18 03:14:16 +0300

    css & add second page & phpMailer & js

--- js/main.js
+++ js/main.js
@@ -1,51 +1,123 @@
-// $('.phone').mask('8 (999) 999-99-99');
+const body = document.querySelector('body'),
+      nav = document.querySelector('nav'),
+      header = document.querySelector('header'),
+      main = document.querySelector('main'),
+      footer = document.querySelector('footer');
 
-// $('#offer-form').validate({
-//   errorClass: "invalid",
-//   errorElement: "div",
-//   rules: {
-//     offerName: {
-//       required: true,
-//       minlength: 2,
-//       maxlength: 15,
-//   },
-//   offerPhone: "required",
-//   offerEmail: {
-//     required: true,
-//     email: true,
-//   }
-// },
-//   messages: {
-//     offerName: {
-//       required: "Заполните поле",
-//       minlength: jQuery.validator.format("Введиет ещё: {0}")
-//       },
-//       offerPhone: "Заполните поле",
-//       offerEmail: {
-//         required: "Заполните поле",
-//         email: "Введите корректный email"
-//       } 
-//   },
-//   submitHandler: function (form) {
-//     $.ajax({
-//       url: 'php/offer.php',
-//       type: 'POST',
-//       data: $(form).serialize(),
-//       success: function(data){
-//         $('.success').html(data);
-//         $('#modal_offer').removeClass('modal_offer_active');
-//         $('nav').removeClass('blur');
-//         $('main').removeClass('blur');
-//         $('header').removeClass('blur');
-//         $('footer').removeClass('blur');
-//         $('body').removeClass('modal-open');
-//         $('.modal-form').addClass('modal-form_active');
-//           setInterval(function(){
-//               $('.modal-form').removeClass('modal-form_active');
-//           }, 3000);
-//         $('#offer-form')[0].reset();
-//       }
-//     });
-//     return false;
-//    }
-// });
+const activeModal = (modal) => {
+  body.style.overflow = 'hidden';
+  modal.style.display = 'flex';
+  nav.classList.add('blur');
+  header.classList.add('blur');
+  main.classList.add('blur');
+  footer.classList.add('blur');
+};
+
+const disableModal = (modal) => {
+  body.style.overflow = 'auto';
+  modal.style.display = 'none';
+  nav.classList.remove('blur');
+  header.classList.remove('blur');
+  main.classList.remove('blur');
+  footer.classList.remove('blur');
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  const slides = document.querySelectorAll('.consul__item'),
+        modalSlider = document.getElementById('modal-slider');
+
+  slides.forEach(elem => {
+    elem.addEventListener('click', (event) => {
+      let target = event.target;
+
+      $('#slider-3').slick('slickGoTo', target.dataset.slickIndex);
+      activeModal(modalSlider);
+    });
+  });
+
+  const scrolling = () => {
+    document.body.addEventListener('click', (event) => {
+      let target = event.target;
+
+      if(target.tagName === 'A' && target.href.indexOf('#') !== -1) {
+        event.preventDefault();
+
+        const blockID = target.getAttribute('href');
+        document.querySelector('' + blockID).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  };
+  scrolling();
+
+  const closeEventListeners = () => {
+    let count = 0,
+        array = [],
+        elem;
+
+    do {
+      count++;
+      elem = document.getElementById(`close${count}`);
+      if(elem !== null) array.push(elem);
+    } while(elem);
+
+    array.forEach(elem => {
+      elem.addEventListener('click', () => {
+        let elemClass = elem.className.replace(/__close/, ''),
+            modal = document.querySelector(`.${elemClass}`);
+
+        disableModal(modal);
+      });
+    });
+  };
+  closeEventListeners();
+
+});
+
+// ---------- JQuery --------------
+
+$(document).ready(function() {
+
+  const modalSuccess = document.getElementById('modal-success');
+
+  $('#phone').mask('8 (999) 999-99-99');
+  $('#form').validate({
+    errorClass: "invalid",
+    errorElement: "div",
+    rules: {
+      name: {
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+    },
+    phone: "required",
+  },
+    messages: {
+      name: {
+        required: "Заполните поле",
+        minlength: jQuery.validator.format("Минимальная длина: {0}")
+      },
+      phone: "Заполните поле"
+    },
+    submitHandler: function (form) {
+      $.ajax({
+        url: 'php/feedback.php',
+        type: 'POST',
+        data: $(form).serialize(),
+        success: function(data) {
+          $('.success').html(data);
+
+          activeModal(modalSuccess);
+          setTimeout(disableModal(modalSuccess), 4000);
+
+          $('#form')[0].reset();
+        }
+      });
+      return false;
+    }
+  });
+});
--- php/feedback.php
+++ php/feedback.php
@@ -4,37 +4,34 @@ $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
 $data = $_POST;
-$name = $_POST['feedbackName'];
-$phone = $_POST['feedbackPhone'];
-$email = $_POST['feedbackEmail'];
+$name = $_POST['name'];
+$phone = $_POST['phone'];
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'for-you-order@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'order1234'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'medical-site-order@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'order4321'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('for-you-order@yandex.ru'); // от кого будет уходить письмо?
-$mail->addAddress('studiodecor_foryou@mail.ru');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+// Ваш любимый киногерой
+// Тони старк
+
+$mail->setFrom('medical-site-order@yandex.ru'); // от кого будет уходить письмо?
+$mail->addAddress('danilabrehov@gmail.com');     // Кому будет уходить письмо 
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Заявка на обратный звонок';
-$mail->Body    = '' .$name . ' оставил(а) заявку, телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->Body    = $name . ' оставил(а) заявку,<br>Телефон: ' .$phone. '.';
 $mail->AltBody = '';
 
+
 if(!$mail->send()) {
-    echo 'Error';
+    echo 'Some error... Повторите попытку.';
 } else {
-  echo $data['feedbackName'] . ', Ваша форма отправлена.';
+  echo $data['name'] . ', Ваша форма отправлена.';
 }
 ?>
